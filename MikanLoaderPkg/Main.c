9e178d053c41c58b4113e6af29ae9d270524be6f
Koheki <mita19941224@gmail.com>
2023-02-07 14:44:04 +0900

    メモリマップを取得しCSVファイルに保持する機能を追加（第２章）

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,12 +1,13 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 
-EFI_STATUS EFIAPI UefiMain(
-    EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table) {
-    Print(L"Hello, Mikan World!\n");
-    while (1);
-    return EFI_SUCCESS;
+struct MemoryMap {
+    UINT buffer_size;
+    VOID* buffer;
+    UINTN map_size;
+    UINTN map_key;
+    UINTN descriptor_size;
+    UINT32 descriptor_version;
 }
 
 EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
@@ -21,4 +22,55 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
         &map->descriptor_size,
         &map->descriptor_version
     );
+}
+
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL *file) {
+    CHAR8 buf[256];
+    UINITN len;
+
+    CHAR8* header = "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+    len = AsciiStrlen(headr);
+    file->Write(file,&len,header);
+
+    Print(L"map->buffer = %08lx, map->map_size = %08x\n",map->buffer, map->map_size);
+
+    EFI_PHYSICAL_ADDRESS iter;
+    int i;
+    for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i=0;
+         iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+         iter += map->descriptor_size, i++) {
+            EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+            len = AsciiPrint(
+                buf, sizeof(buf),
+                "%u, %x, %-ls, %08lx, %lx, %lx\n",
+                i, desc->Type, GetMemoryTypeUnicode(desc->Type),
+                desc->PhysicalStart, desc->NumberOfPages, desc->Attribute&0xffffflu);
+            file->Write(file, &len, buf);
+         }
+         return EFI_SUCCESS;
+}
+
+EFI_STATUS EFIAPI UefiMain(
+    EFI_HANDLE image_handle,
+    EFI_SYSTEM_TABLE *system_table) {
+    Print(L"Hello, Mikan World!\n");
+    
+    CHAR8 memmap_buf[4096*4];
+    struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+    GetMemoryMap(&memmap);
+    
+    EFI_FILE_PROTOCOL* root_dir;
+    OpenRootDir(image_handle, &root_dir);
+
+    EFI_FILE_PROTOCOL* memmap_file;
+
+    root_dir->Open(
+        root_dir, &memmap_file, L"\\memmap",
+        EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+    
+    SaveMemoryMap(&memmap, memmap_file);
+    memmap_file->Close(memmap_file);
+
+    while (1);
+    return EFI_SUCCESS;
 }
